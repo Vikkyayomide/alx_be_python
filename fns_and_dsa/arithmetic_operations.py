453ffed9073cf89d49343a37e2d3f2b68545f348
vikky <olawunmi886@gmail.com>
2025-07-27 04:44:33 +0100

    Add perform_operation function for arithmetic operations task

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,24 @@
+def perform_operation(num1: float, num2: float, operation: str):
+    """
+    Performs a basic arithmetic operation on two numbers.
+
+    Parameters:
+    - num1 (float): The first number
+    - num2 (float): The second number
+    - operation (str): The operation to perform: 'add', 'subtract', 'multiply', or 'divide'
+
+    Returns:
+    - float or str: Result of the arithmetic operation or an error message
+    """
+    if operation == "add":
+        return num1 + num2
+    elif operation == "subtract":
+        return num1 - num2
+    elif operation == "multiply":
+        return num1 * num2
+    elif operation == "divide":
+        if num2 == 0:
+            return "Error: Division by zero"
+        return num1 / num2
+    else:
+        return "Error: Invalid operation"
