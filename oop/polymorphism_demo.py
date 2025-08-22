67621b3e284d2fb2399a43d3a9c9e2cd4068aafc
vikky <olawunmi886@gmail.com>
2025-08-22 15:40:37 +0100

    Polymorphism

---
+++ oop/polymorphism_demo.py
@@ -0,0 +1,26 @@
+# oop/polymorphism_demo.py
+import math
+
+class Shape:
+    def area(self):
+        """Base method to be overridden by subclasses"""
+        raise NotImplementedError("Subclasses must override the area() method")
+
+
+class Rectangle(Shape):
+    def __init__(self, length: float, width: float):
+        self.length = length
+        self.width = width
+
+    def area(self):
+        """Calculate area of rectangle: length × width"""
+        return self.length * self.width
+
+
+class Circle(Shape):
+    def __init__(self, radius: float):
+        self.radius = radius
+
+    def area(self):
+        """Calculate area of circle: π × radius²"""
+        return math.pi * (self.radius ** 2)
